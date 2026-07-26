4c83da6a4cd26b9886c4fdfa9c30cec8eb9485bf
Nicola Tesio <ntesio4@gmail.com>
2026-07-26 18:21:17 -0300

    se sube cambio a database

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 
 const { DB_PROTOCOL, DB_HOST, DB_PASS, DB_USER, DB_OPTIONS, DB_NAME } =
   process.env;
-const MONGODB_URI = `${DB_PROTOCOL}://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?${DB_OPTIONS}`;
-
+//const MONGODB_URI = `${DB_PROTOCOL}://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?${DB_OPTIONS}`;
+const MONGODB_URI = 
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI);
